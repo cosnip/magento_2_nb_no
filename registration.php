5d3160a5bee099ee9d4e6079d6e82c1822a27c2c
cosnip <32839056+cosnip@users.noreply.github.com>
2017-10-16 15:59:13 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'cosnip_magento_2_nb_no',
+    'cosnip_nb_no',
     __DIR__
 );
